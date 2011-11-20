feb78edf147055e741097c3f0d7a7f8e84b8c8ca
lucascomputacao@gmail.com <lucascomputacao@gmail.com@23b1cb1e-4464-a9ce-1666-a9e8b1f53545>
2011-11-20 03:06:31 +0000

    construtor, getters e setters da classe funcionarios

--- trabalhoPOO/src/trabalhopoo/Funcionarios.java
+++ trabalhoPOO/src/trabalhopoo/Funcionarios.java
@@ -12,5 +12,63 @@ public class Funcionarios {
     private String nome, endereco, telefone, dataAdmissao;
     private int id;
     private float salario;
+
+    public Funcionarios(String nome, String endereco, String telefone, String dataAdmissao, int id, float salario) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.dataAdmissao = dataAdmissao;
+        this.id = id;
+        this.salario = salario;
+    }
+
+    public String getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(String dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
     
 }
