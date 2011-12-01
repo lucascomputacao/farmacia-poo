e1ffd53d6d61fcee8706aebe75c06701f1ef5e1c
lucascomputacao@gmail.com <lucascomputacao@gmail.com@23b1cb1e-4464-a9ce-1666-a9e8b1f53545>
2011-12-01 21:16:07 +0000

    recolocando classe perfumaria

---
+++ Farmacia/src/farmacia/Perfumaria.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmacia;
+
+/**
+ *
+ * @author lucas
+ */
+public class Perfumaria extends Item {
+
+    private String fragrancia,  unidade;//unidade = vendido em caixa[sabonetes] ou em mls [perfumes]
+    private int codPerf;
+
+    public Perfumaria(String lote, float preco,
+            String validade, String nome, String fragrancia, String unidade) {
+        super(lote, preco, validade, nome);
+        this.fragrancia = fragrancia;
+        this.unidade = unidade;
+        this.codPerf = getCodigo();//pega o código sequencial para novo item cadastrado 
+    }
+    
+    
+
+    @Override
+    void imprimeInfo() {   
+        System.out.println("Codigo: " + getCodPerf() + "\n"
+                + "Nome: " + getNome() + "\n"
+                + "Fragrancia: " + getFragrancia()+"\n"
+                + "Fragrancia: " + getUnidade()+"\n"
+                + "Lote: " + getLote() + "\n"
+                + "Validade: " + getValidade()+ "\n"
+                + "Preco: " + getPreco());
+    }
+
+    public int getCodPerf() {
+        return codPerf;
+    }
+
+    public void setCodPerf(int codPerf) {
+        this.codPerf = codPerf;
+    }
+
+    public String getFragrancia() {
+        return fragrancia;
+    }
+
+    public void setFragrancia(String fragrancia) {
+        this.fragrancia = fragrancia;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+    
+}
